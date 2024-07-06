c13706532fcfff592116361983cb7ef8a5b8a08e
vegita24 <sahil242saini@gmail.com>
2024-07-06 11:50:12 +0530

    splitwise app

---
+++ splitwise/src/test/java/com/example/splitwise/SplitWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splitwise;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SplitWiseApplicationTests {
+
+	@Test
+    public void contextLoads() {
+	}
+
+}
